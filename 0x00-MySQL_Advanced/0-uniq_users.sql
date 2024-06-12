5a963e801274c0bc5b98dbb9597cdfc1089f5256
khaledolemy <khaledolemy@gmail.com>
2024-06-12 09:50:30 +0300

    fix create script

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
 -- Creates a table and handles if exists
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
