44218f76b6b4d70c15df54ec332aa64522bede31
khaledolemy <khaledolemy@gmail.com>
2024-06-13 23:47:19 +0300

    fix

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,8 @@ CREATE TRIGGER IF NOT EXISTS decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    UPDATE items SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
 END $$
 DELIMITER ;
