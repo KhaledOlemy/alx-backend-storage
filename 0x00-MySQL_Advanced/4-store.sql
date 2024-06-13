2c39b1fad1cd24a84fd94e99ff1bbcdafe60c45b
khaledolemy <khaledolemy@gmail.com>
2024-06-13 23:48:44 +0300

    if not

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,7 @@
 -- create trigger to descrease item quantities when orders made
 DROP TRIGGER IF EXISTS decrease_quantity;
 DELIMITER $$
-CREATE TRIGGER IF NOT EXISTS decrease_quantity
+CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
