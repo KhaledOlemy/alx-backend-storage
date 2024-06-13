5502fdc22e8be5a85d8d7bbaa3e794da08eadf55
khaledolemy <khaledolemy@gmail.com>
2024-06-13 23:55:51 +0300

    EMAIL VALIDATOR

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- valid email trigger
+DROP TRIGGER IF EXISTS validChecker;
+DELIMITER $$
+CREATE TRIGGER validChecker
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email
+    THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
