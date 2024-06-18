cf88194f639e7c86c2e28fa3000e208bd5be3ebe
khaledolemy <khaledolemy@gmail.com>
2024-06-19 00:29:18 +0300

    status report

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Status Report"""
+from pymongo import MongoClient
+
+
+def generate_status():
+    """Generate a status report"""
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    targetCollection = client.logs.nginx
+    nOfRequests = targetCollection.count_documents()
+    print(f"{nOfRequests} logs")
+    print("Methods:")
+    for method in methods:
+        methodRequestCount = targetCollection.find({"method": method})
+        print("method {}: {}".format(method, methodRequestCount))
+    statusChecks = targetCollection.find({"method": "GET", "path": "/status"})
+    print(f"{len(statusChecks)} status check")
