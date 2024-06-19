13ea4236b77d515abea50c219dc22602ac5d32e7
khaledolemy <khaledolemy@gmail.com>
2024-06-19 19:15:26 +0300

    task0 file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,12 +3,16 @@
 import redis
 from uuid import uuid4
 
+
 class Cache:
+    """Cache class"""
     def __init__(self) -> None:
+        """Initializer"""
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     def store(self, data: str | int | float | bytes) -> str:
+        """stores a value in a new ID and returns the ID"""
         newID = str(uuid4())
         self._redis.set(newID, data)
         return newID
