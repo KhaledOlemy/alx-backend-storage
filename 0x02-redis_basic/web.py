233be58decd618c9548562a25addd34bd4aa3f0a
khaledolemy <khaledolemy@gmail.com>
2024-06-20 05:58:47 +0300

    trial

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
-"""Tracks and caches HTML content"""
+""" Redis Module """
 
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
 
 redis_ = redis.Redis()
 
 
-def counter(method: Callable) -> Callable:
-    """Returns wrapper function"""
-
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def methodWrapper(url):
-        """Wrapper function"""
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
         redis_.incr(f"count:{url}")
-        cachedResponse = redis_.get(f"cached:{url}")
-        if cachedResponse:
-            return cachedResponse.decode("UTF-8")
-        else:
-            response = method(url)
-            redis_.setex(f"cached:{url}", 10, response)
-            return response
-    return methodWrapper
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
 
 
-@counter
-def getPage(url: str) -> str:
-    """Get the HTML content and return text"""
-    return requests.get(url).text
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
